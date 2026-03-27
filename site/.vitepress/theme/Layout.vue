23a3b0af33c1a594cb9d3afe0c29904089de1e44
Danger <144523105+DangerDrome@users.noreply.github.com>
2026-03-27 23:33:38 +1300

    fix(site): larger icons, nowrap meta text on download cards

--- site/.vitepress/theme/Layout.vue
+++ site/.vitepress/theme/Layout.vue
@@ -15,7 +15,7 @@ const winUrl = `https://github.com/megasupersoft/Ghosted/releases/latest`
         <a :href="macUrl" class="download-btn download-btn--primary">
           <div class="download-btn__icon">
             <!-- Simple Icons: Apple -->
-            <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="currentColor">
+            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="currentColor">
               <path d="M12.152 6.896c-.948 0-2.415-1.078-3.96-1.04-2.04.027-3.91 1.183-4.961 3.014-2.117 3.675-.546 9.103 1.519 12.09 1.013 1.454 2.208 3.09 3.792 3.039 1.52-.065 2.09-.987 3.935-.987 1.831 0 2.35.987 3.96.948 1.637-.026 2.676-1.48 3.676-2.948 1.156-1.688 1.636-3.325 1.662-3.415-.039-.013-3.182-1.221-3.22-4.857-.026-3.04 2.48-4.494 2.597-4.559-1.429-2.09-3.623-2.324-4.39-2.376-2-.156-3.675 1.09-4.61 1.09zM15.53 3.83c.843-1.012 1.4-2.427 1.245-3.83-1.207.052-2.662.805-3.532 1.818-.78.896-1.454 2.338-1.273 3.714 1.338.104 2.715-.688 3.559-1.701"/>
             </svg>
           </div>
@@ -27,7 +27,7 @@ const winUrl = `https://github.com/megasupersoft/Ghosted/releases/latest`
         <a :href="linuxUrl" class="download-btn download-btn--primary">
           <div class="download-btn__icon">
             <!-- Material Design Icons: Penguin (Tux) -->
-            <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="currentColor">
+            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="currentColor">
               <path d="M19,16C19,17.72 18.37,19.3 17.34,20.5C17.75,20.89 18,21.41 18,22H6C6,21.41 6.25,20.89 6.66,20.5C5.63,19.3 5,17.72 5,16H3C3,14.75 3.57,13.64 4.46,12.91L4.47,12.89C6,11.81 7,10 7,8V7A5,5 0 0,1 12,2A5,5 0 0,1 17,7V8C17,10 18,11.81 19.53,12.89L19.54,12.91C20.43,13.64 21,14.75 21,16H19M16,16A4,4 0 0,0 12,12A4,4 0 0,0 8,16A4,4 0 0,0 12,20A4,4 0 0,0 16,16M10,9L12,10.5L14,9L12,7.5L10,9M10,5A1,1 0 0,0 9,6A1,1 0 0,0 10,7A1,1 0 0,0 11,6A1,1 0 0,0 10,5M14,5A1,1 0 0,0 13,6A1,1 0 0,0 14,7A1,1 0 0,0 15,6A1,1 0 0,0 14,5Z"/>
             </svg>
           </div>
@@ -39,7 +39,7 @@ const winUrl = `https://github.com/megasupersoft/Ghosted/releases/latest`
         <a :href="winUrl" class="download-btn download-btn--secondary">
           <div class="download-btn__icon">
             <!-- Simple Icons: Windows -->
-            <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" viewBox="0 0 24 24" fill="currentColor">
+            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="currentColor">
               <path d="M0,0H11.377V11.372H0ZM12.623,0H24V11.372H12.623ZM0,12.623H11.377V24H0Zm12.623,0H24V24H12.623"/>
             </svg>
           </div>
@@ -126,8 +126,8 @@ const winUrl = `https://github.com/megasupersoft/Ghosted/releases/latest`
   align-items: center;
   justify-content: center;
   flex-shrink: 0;
-  width: 48px;
-  height: 48px;
+  width: 56px;
+  height: 56px;
   border-radius: 12px;
   background: rgba(255, 255, 255, 0.15);
 }
@@ -153,11 +153,12 @@ const winUrl = `https://github.com/megasupersoft/Ghosted/releases/latest`
 }
 
 .download-btn__meta {
-  font-size: 12px;
+  font-size: 11px;
   font-weight: 400;
   letter-spacing: 0.02em;
   line-height: 1.3;
   opacity: 0.7;
   font-family: var(--vp-font-family-mono);
+  white-space: nowrap;
 }
 </style>
